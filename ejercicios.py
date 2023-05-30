40e0d8db6f40b24ead7ef9f729c34eb1d85fe2de
Diana Soledad <dayaba2413@hotmail.com>
2023-05-30 15:41:59 -0500

    Ejercicios con python

---
+++ ejercicios.py
@@ -0,0 +1,22 @@
+"""def derechos_humanos():
+    print("Derecho ala v ida")
+    print("derecho a la educacion")
+    print("derecho a ser")
+
+derechos_humanos()
+
+
+
+
+def bienvenida():
+    print('Binevenidad a python')
+    return
+
+bienvenida()
+
+
+def bienvenida():
+    print('Binevenida a python', nombre + '!')
+    
+bienvenida('Diaana')
+"""
